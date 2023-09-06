3862f78b1e87f8df7c6a62fa4aaaecf8fb4e9242
Florescu Cristian <qflorescucristian@gmail.com>
2023-09-06 11:19:33 +0300

    Added Median of Two Sorted Arrays challenge.

---
+++ csharp-median-of-two-sorted-arrays/csharp-median-of-two-sorted-arrays/Program.cs
@@ -0,0 +1,11 @@
+﻿using csharp_median_of_two_sorted_arrays;
+
+internal class Program
+{ 
+    private static void Main(string[] args)
+    {
+        int[] nums1 = new int[] { 1, 3 };
+        int[] nums2 = new int[] { 2, 7 };
+        Console.WriteLine(Solution.FindMedianSortedArrays(nums1, nums2));
+    }
+}
---
+++ csharp-median-of-two-sorted-arrays/csharp-median-of-two-sorted-arrays/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_median_of_two_sorted_arrays
+{
+    public class Solution
+    {
+        public static Double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] numbers = nums1.Concat(nums2).OrderBy(n => n).ToArray();
+            if (numbers.Count() % 2 == 0)
+            {
+                return ((double)numbers[numbers.Length / 2] + numbers[numbers.Length / 2 - 1]) / 2;
+            }
+            return numbers[numbers.Length / 2];
+        }
+    }
+}
