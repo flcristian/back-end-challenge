f6e8185fb18308b9e4ce692c4939376f6a11c14a
Florescu Cristian <qflorescucristian@gmail.com>
2023-09-14 13:31:16 +0300

    Insert greatest common divisor in linked list challenge

---
+++ insert-greatest-common-divisors-in-linked-list/README.md
@@ -0,0 +1,3 @@
+![Problem](https://github.com/flcristian/back-end-challenges/blob/master/csharp-median-of-two-sorted-arrays/problem.png)
+![Solution](https://github.com/flcristian/back-end-challenges/blob/master/csharp-median-of-two-sorted-arrays/solution.png)
+![Result](https://github.com/flcristian/back-end-challenges/blob/master/csharp-median-of-two-sorted-arrays/result.png)
---
+++ insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+internal class Program
+{
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public int GreatestCommonDivisor(int x, int y)
+        {
+            while(x != y)
+            {
+                if(x > y)
+                {
+                    x -= y;
+                }
+                else
+                {
+                    y -= x;
+                }
+            }
+
+            return x;
+        }
+
+        public ListNode InsertGreatestCommonDivisors(ListNode head)
+        {
+            ListNode node = head;
+
+            while (node.next != null)
+            {
+                ListNode toAdd = new ListNode(GreatestCommonDivisor(node.val, node.next.val), node.next);
+                node.next = toAdd;
+
+                node = node.next.next;
+            }
+
+            return head;
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        int[] nr = { 18, 6, 10, 3 };
+        ListNode head = null;
+        int i = nr.Length - 1;
+        while(i > -1)
+        {
+            head = new ListNode(nr[i], head);
+            i--;
+        }
+
+        Solution sol = new Solution();
+        sol.InsertGreatestCommonDivisors(head);
+
+        while (head != null)
+        {
+            Console.WriteLine(head.val);
+            head = head.next;
+        }
+    }
+}
