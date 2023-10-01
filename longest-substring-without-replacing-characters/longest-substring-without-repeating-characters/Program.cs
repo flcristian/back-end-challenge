6bd34a46e3d09056f51065779ebc9d50c8de43ab
flcristian <qflorescucristian@gmail.com>
2023-10-01 19:03:20 +0300

    Added longest substring without repeating characters challenge

---
+++ longest-substring-without-replacing-characters/longest-substring-without-repeating-characters/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace longest_substring_without_repeating_characters;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+        Console.WriteLine(sol.LengthOfLongestSubstring("pwwkew"));
+    }
+}
