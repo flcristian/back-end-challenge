1000b6be170ee9d9d59af033b1750fb936e6d893
flcristian <qflorescucristian@gmail.com>
2023-10-01 23:28:21 +0300

    Corrected solution, removed usage of 64-bit integers

--- reverse-integer/Solution.h
+++ reverse-integer/Solution.h
@@ -8,8 +8,8 @@ public:
         bool negative = x < 0;
         int result = 0;
         while(x != 0){
-            if((long)result * 10 + x % 10 > 2147483646 || (long)result * 10 + x % 10 < -2147483647) return 0;
-            result = result * 10 + x % 10;
+            if(result > (INT_MAX - abs(x % 10)) / 10 || result < (INT_MIN + abs(x % 10)) / 10) return 0;
+            result = result * 10 + abs(x % 10);
             x /= 10;
         }
         if(negative && result > 0) result *= -1;
