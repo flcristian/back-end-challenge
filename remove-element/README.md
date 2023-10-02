9e560293eca775d20e6d07c61568286dfa057f73
flcristian <qflorescucristian@gmail.com>
2023-10-02 18:48:38 +0300

    Added remove element challenge

---
+++ remove-element/README.md
@@ -0,0 +1,3 @@
+![Problem](https://github.com/flcristian/back-end-challenges/blob/master/remove-element/problem.png)
+![Solution](https://github.com/flcristian/back-end-challenges/blob/master/remove-element/solution.png)
+![Result](https://github.com/flcristian/back-end-challenges/blob/master/remove-element/result.png)
---
+++ remove-element/remove-element/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace remove_element;
+
+public class Solution {
+    public int RemoveElement(int[] nums, int val)
+    {
+        int i = 0, k = nums.Length;
+        while (i < k)
+        {
+            if (nums[i] == val)
+            {
+                nums[i] = nums[k - 1];
+                k--;
+            }
+            else i++;
+        }
+
+        return k;
+    }
+}
