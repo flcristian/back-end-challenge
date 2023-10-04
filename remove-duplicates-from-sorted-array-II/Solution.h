0aaa78f26cf73f1fd54450a6510923266a8afb22
flcristian <qflorescucristian@gmail.com>
2023-10-04 12:39:25 +0300

    Added remove duplicates from sorted array II challenge

---
+++ remove-duplicates-from-sorted-array-II/Solution.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int freq[201]{}, result[(const int)size(nums)];
+        int k = 0;
+        for(int i = 0; i < size(nums);i++){
+            freq[nums[i] + 100]++;
+            if(freq[nums[i] + 100] <= 2) result[k++] = nums[i];
+        }
+        for(int i = 0;i<k;i++){
+            nums[i] = result[i];
+        }
+        return k;
+    }
+};
--- remove-duplicates-from-sorted-array/Solution.h
+++ remove-duplicates-from-sorted-array/Solution.h
@@ -6,11 +6,17 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int freq[201]{}, result[(const int)size(nums)];
-        int k = 0;
-        for(int i = 0; i < size(nums);i++){
-            freq[nums[i] + 100]++;
-            if(freq[nums[i] + 100] == 1) result[k++] = nums[i];
+        int c = 1, k = 0, result[(const int)size(nums)];
+        if(size(nums) > 0){
+            result[k] = nums[0];
+            k++;
+        }
+
+        for(int i = 1; i < size(nums);i++){
+            if(nums[i] == nums[i-1]) c++;
+            else c = 1;
+
+            if(c <= 2) result[k++] = nums[i];
         }
         for(int i = 0;i<k;i++){
             nums[i] = result[i];
