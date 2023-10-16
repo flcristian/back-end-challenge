37ca60238a0b2de4d78288eb5e7168a1ef1354f8
flcristian <qflorescucristian@gmail.com>
2023-10-16 09:55:35 +0300

    Added majority element challenge

---
+++ majority-element/README.md
@@ -0,0 +1,3 @@
+![Problem](https://github.com/flcristian/back-end-challenges/blob/master/majority-element/problem.png)
+![Solution](https://github.com/flcristian/back-end-challenges/blob/master/majority-element/solution.png)
+![Result](https://github.com/flcristian/back-end-challenges/blob/master/majority-elementp/result.png)
---
+++ majority-element/majority-element/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace majority_element
+{
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int element = nums[0], count = 1;
+            int i = 1;
+            while(i < nums.Length && count <= nums.Length / 2)
+            {
+                if (count == 0)
+                {
+                    element = nums[i];
+                }
+                if (element == nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+                i++;
+            }
+            return element;
+        }
+    }
+}
