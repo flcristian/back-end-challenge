dfe9427bbddefd4bddb2b46c219d45594d26fe18
flcristian <qflorescucristian@gmail.com>
2023-11-03 16:23:25 +0200

    Added best time to buy and sell stock challenge

---
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock/Program.cs
@@ -0,0 +1,11 @@
+﻿using best_time_to_buy_and_sell_stock;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+        int[] prices = { 7,6,4,3,1 };
+        Console.WriteLine(sol.MaxProfit(prices));
+    }
+}
