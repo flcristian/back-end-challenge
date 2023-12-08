55a9c89d7656600b8148de8fac5e3c2a7da12756
flcristian <qflorescucristian@gmail.com>
2023-12-08 08:54:02 +0200

    Added merge sorted lists challenge

---
+++ merge-sorted-lists/README.md
@@ -0,0 +1,3 @@
+![Problem](https://github.com/flcristian/back-end-challenges/blob/master/merge-sorted-lists/problem.png)
+![Solution](https://github.com/flcristian/back-end-challenges/blob/master/merge-sorted-lists/solution.png)
+![Result](https://github.com/flcristian/back-end-challenges/blob/master/merge-sorted-lists/result.png)
---
+++ merge-sorted-lists/merge-sorted-lists/Program.cs
@@ -0,0 +1,19 @@
+﻿using merge_sorted_lists;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4, null)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4, null)));
+        Solution sol = new Solution();
+        ListNode result = sol.MergeTwoLists(list1, list2);
+
+        ListNode head = result;
+        while (head != null)
+        {
+            Console.Write(head.val + " ");
+            head = head.next;
+        }
+    }
+}
